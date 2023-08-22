b9c7965c15ac5efeab78b3b073ea1b12591439d6
hkxpz <2446392171@qq.com>
2023-08-22 14:33:21 +0800

    限流插件

--- week03/webook/pkg/ginx/middleware/ratelimit/slide_window.lua
+++ week03/webook/pkg/ginx/middleware/ratelimit/slide_window.lua
@@ -1,19 +1,25 @@
---限流对象
+-- 1, 2, 3, 4, 5, 6, 7 这是你的元素
+-- ZREMRANGEBYSCORE key1 0 6
+-- 7 执行完之后
+
+-- 限流对象
 local key = KEYS[1]
---窗口大小
+-- 窗口大小
 local window = tonumber(ARGV[1])
---阈值
-local threshold = tonumber(ARGV[2])
+-- 阈值
+local threshold = tonumber( ARGV[2])
 local now = tonumber(ARGV[3])
---窗口起始时间
+-- 窗口的起始时间
 local min = now - window
 
-redis.call("ZREMRANGEBYSCORE", key, "-inf", min)
-local cnt = redis.call("ZCOUNT", key, "-inf", "+inf")
-if cnt > threshold then
+redis.call('ZREMRANGEBYSCORE', key, '-inf', min)
+local cnt = redis.call('ZCOUNT', key, '-inf', '+inf')
+if cnt >= threshold then
+    -- 执行限流
     return "true"
 else
-    redis.call("ZADD", key, now, now)
-    redis.call("PEXPIRE", key, window)
-    return false
+    -- 把 score 和 member 都设置成 now
+    redis.call('ZADD', key, now, now)
+    redis.call('PEXPIRE', key, window)
+    return "false"
 end
