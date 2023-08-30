6e4a2a1dbf67aeefeb252a42a86b9102a77c039f
hkxpz <2446392171@qq.com>
2023-08-30 16:02:41 +0800

    UserService 单元测试

--- webook/internal/service/user_test.go
+++ webook/internal/service/user_test.go
@@ -2,8 +2,13 @@ package service
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
@@ -12,6 +17,60 @@ import (
 	"geektime-basic-go/webook/internal/repository"
 	"geektime-basic-go/webook/internal/repository/mocks"
 )
+
+func TestUserService_Signup(t *testing.T) {
+	testCases := []struct {
+		name    string
+		mock    func(ctrl *gomock.Controller) repository.UserRepository
+		ctx     context.Context
+		user    domain.User
+		wantErr error
+	}{
+		{
+			name: "注册成功",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
+				return repo
+			},
+			ctx:     context.Background(),
+			user:    domain.User{Password: "123123"},
+			wantErr: nil,
+		},
+		{
+			name: "加密失败",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				return nil
+			},
+			ctx:     context.Background(),
+			user:    domain.User{Password: "你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好"},
+			wantErr: bcrypt.ErrPasswordTooLong,
+		},
+		{
+			name: "注册失败",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(errors.New("模拟注册失败"))
+				return repo
+			},
+			ctx:     context.Background(),
+			user:    domain.User{Password: "123123"},
+			wantErr: errors.New("模拟注册失败"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			repo := tc.mock(ctrl)
+			svc := NewUserService(repo)
+			err := svc.Signup(tc.ctx, tc.user)
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
 
 func TestUserService_Login(t *testing.T) {
 	now := time.Now()
@@ -61,12 +120,23 @@ func TestUserService_Login(t *testing.T) {
 			name: "未找到用户",
 			mock: func(ctrl *gomock.Controller) repository.UserRepository {
 				repo := mocks.NewMockUserRepository(ctrl)
-				repo.EXPECT().FindByEmail(context.Background(), "123@qq.com").Return(domain.User{}, ErrInvalidUserOrPassword)
+				repo.EXPECT().FindByEmail(context.Background(), "123@qq.com").Return(domain.User{}, repository.ErrUserNotFound)
 				return repo
 			},
 			ctx:     context.Background(),
 			email:   "123@qq.com",
 			wantErr: ErrInvalidUserOrPassword,
+		},
+		{
+			name: "查找用户失败",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().FindByEmail(context.Background(), "123@qq.com").Return(domain.User{}, errors.New("模拟查找用户失败"))
+				return repo
+			},
+			ctx:     context.Background(),
+			email:   "123@qq.com",
+			wantErr: errors.New("模拟查找用户失败"),
 		},
 		{
 			name: "密码错误",
@@ -100,6 +170,172 @@ func TestUserService_Login(t *testing.T) {
 			svc := NewUserService(repo)
 			user, err := svc.Login(tc.ctx, tc.email, tc.password)
 			assert.Equal(t, tc.wantErr, err)
+			assert.Equal(t, tc.wantUser, user)
+		})
+	}
+}
+
+func TestUserService_Profile(t *testing.T) {
+	testCases := []struct {
+		name     string
+		mock     func(ctrl *gomock.Controller) repository.UserRepository
+		ctx      context.Context
+		id       int64
+		wantUser domain.User
+		wantErr  error
+	}{
+		{
+			name: "获取成功",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().FindByID(gomock.Any(), gomock.Any()).Return(domain.User{Id: 1}, nil)
+				return repo
+			},
+			ctx:      context.Background(),
+			id:       1,
+			wantUser: domain.User{Id: 1},
+			wantErr:  nil,
+		},
+		{
+			name: "获取失败",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().FindByID(gomock.Any(), gomock.Any()).Return(domain.User{}, errors.New("模拟创建失败"))
+				return repo
+			},
+			ctx:      context.Background(),
+			id:       1,
+			wantUser: domain.User{},
+			wantErr:  errors.New("模拟创建失败"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			repo := tc.mock(ctrl)
+			svc := NewUserService(repo)
+			user, err := svc.Profile(tc.ctx, tc.id)
+			assert.Equal(t, tc.wantErr, err)
+			assert.Equal(t, tc.wantUser, user)
+		})
+	}
+}
+
+func TestUserService_Edit(t *testing.T) {
+	testCases := []struct {
+		name    string
+		mock    func(ctrl *gomock.Controller) repository.UserRepository
+		ctx     context.Context
+		user    domain.User
+		wantErr error
+	}{
+		{
+			name: "更新成功",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
+				return repo
+			},
+			ctx:     context.Background(),
+			user:    domain.User{},
+			wantErr: nil,
+		},
+		{
+			name: "获取失败",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().Update(gomock.Any(), gomock.Any()).Return(errors.New("模拟更新失败"))
+				return repo
+			},
+			ctx:     context.Background(),
+			user:    domain.User{},
+			wantErr: errors.New("模拟更新失败"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			repo := tc.mock(ctrl)
+			svc := NewUserService(repo)
+			err := svc.Edit(tc.ctx, tc.user)
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
+
+func TestUserService_FindOrCreate(t *testing.T) {
+	testCases := []struct {
+		name     string
+		mock     func(ctrl *gomock.Controller) repository.UserRepository
+		ctx      context.Context
+		phone    string
+		wantUser domain.User
+		wantErr  error
+	}{
+		{
+			name: "找到用户",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().FindByPhone(gomock.Any(), gomock.Any()).Return(domain.User{Phone: "13888888888"}, nil)
+				return repo
+			},
+			ctx:      context.Background(),
+			phone:    "13888888888",
+			wantUser: domain.User{Phone: "13888888888"},
+		},
+		{
+			name: "查找用户失败",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().FindByPhone(gomock.Any(), gomock.Any()).Return(domain.User{}, errors.New("模拟查找用户失败"))
+				return repo
+			},
+			ctx:     context.Background(),
+			phone:   "13888888888",
+			wantErr: errors.New("模拟查找用户失败"),
+		},
+		{
+			name: "没找到用户,创建成功",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().FindByPhone(gomock.Any(), gomock.Any()).Return(domain.User{}, repository.ErrUserNotFound)
+				repo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
+				repo.EXPECT().FindByPhone(gomock.Any(), gomock.Any()).Return(domain.User{Phone: "13888888888"}, nil)
+				return repo
+			},
+			ctx:      context.Background(),
+			phone:    "13888888888",
+			wantUser: domain.User{Phone: "13888888888"},
+		},
+		{
+			name: "没找到用户,没创建成功",
+			mock: func(ctrl *gomock.Controller) repository.UserRepository {
+				repo := mocks.NewMockUserRepository(ctrl)
+				repo.EXPECT().FindByPhone(gomock.Any(), gomock.Any()).Return(domain.User{}, repository.ErrUserNotFound)
+				repo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(errors.New("模拟创建失败"))
+				return repo
+			},
+			ctx:     context.Background(),
+			phone:   "13888888888",
+			wantErr: errors.New("模拟创建失败"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			repo := tc.mock(ctrl)
+			svc := NewUserService(repo)
+			user, err := svc.FindOrCreate(tc.ctx, tc.phone)
+			require.Equal(t, tc.wantErr, err)
 			assert.Equal(t, tc.wantUser, user)
 		})
 	}
